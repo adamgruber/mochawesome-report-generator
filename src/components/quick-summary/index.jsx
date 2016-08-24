623159ef2ecc530e8c31bad73a0e2e5c1d899591
Adam Gruber <adam.gruber@dowjones.com>
2016-08-24 11:12:56 -0400

    remove links from quick summary

--- src/components/quick-summary/index.jsx
+++ src/components/quick-summary/index.jsx
@@ -12,12 +12,15 @@ const QuickSummary = observer(({ stats }) => {
   return (
     <div className={ cx('cnt', { show: reportStore.showQuickSummary }) }>
       <ul className={ cx('list-unstyled', 'list') }>
-        <li className={ cx('item', 'duration') } title='Duration'>{ formatSummaryDuration(duration) }<span>{ getSummaryDurationUnits(duration) }</span></li>
+        <li className={ cx('item', 'duration') } title='Duration'>
+          { formatSummaryDuration(duration) }
+          <span>{ getSummaryDurationUnits(duration) }</span>
+        </li>
         <li className={ cx('item', 'suites') } title='Suites'>{ suites }</li>
         <li className={ cx('item', 'tests') } title='Tests'>{ testsRegistered }</li>
-        <li className={ cx('item', 'passes') } title='Passed' data-filter='passed'>{ passes }</li>
-        <li className={ cx('item', 'failures') } title='Failed' data-filter='failed'>{ failures }</li>
-        <li className={ cx('item', 'pending') } title='Pending' data-filter='pending'>{ pending }</li>
+        <li className={ cx('item', 'passes') } title='Passed'>{ passes }</li>
+        <li className={ cx('item', 'failures') } title='Failed'>{ failures }</li>
+        <li className={ cx('item', 'pending') } title='Pending'>{ pending }</li>
       </ul>
     </div>
   );
