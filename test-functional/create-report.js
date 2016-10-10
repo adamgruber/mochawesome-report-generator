4993a6070519e53cfac5ab707df165063be5626f
Adam Gruber <adam.gruber@dowjones.com>
2016-10-10 10:24:33 -0400

    test stuff

--- test-functional/create-report.js
+++ test-functional/create-report.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const report = require('../lib/main');
 const data = require('./test-data.json');
+// const data = require('./test-data-single.json');
 
 report.createSync(data, {
   dev: true,
