434cf377f8cbc17c1a7df5fe388eaed1749492ec
Adam Gruber <talknmime@gmail.com>
2016-11-14 17:11:15 -0500

    remove test-functional stuff; use cli instead

--- test-functional/create-report.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const report = require('../lib/main');
-const data = require('./test-data.json');
-// const data = require('./test-data-single.json');
-
-report.createSync(data, {
-  dev: true,
-  reportHtmlFile: path.join(__dirname, 'reportTest.html')
-});
