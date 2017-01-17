dc1cf5fb78b75d4f20285d8bbe52828729e949dc
Maggie Jones <jones@cleverbridge.com>
2017-01-17 00:00:23 -0600

    expanding image

--- src/components/test/context.jsx
+++ src/components/test/context.jsx
@@ -11,6 +11,14 @@ const protocolRegEx = /^(?:(?:https?|ftp):\/\/)/i;
 const urlRegEx = /^(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/ // eslint-disable-line
 
 class TestContext extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { clicked: false };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
   static displayName = 'TestContext';
 
   static propTypes = {
@@ -26,6 +34,7 @@ class TestContext extends Component {
     const isImage = imgRegEx.test(url);
     const hasProtocol = protocolRegEx.test(url);
     const cxname = isImage ? 'image-link' : 'text-link';
+    const cximage = cx('image');
     const linkUrl = `${hasProtocol ? '' : 'http://'}${url}`;
     return (
       <a
@@ -35,7 +44,11 @@ class TestContext extends Component {
         target='_blank'
         alt={ title } >
         { isImage
-          ? <img src={ linkUrl } className={ cx('image') } role='presentation' />
+          ? <img
+            src={ linkUrl }
+            className={ cximage }
+            role='presentation'
+            onClick={ this.handleClick } />
           : url
         }
       </a>
@@ -43,14 +56,23 @@ class TestContext extends Component {
   };
 
   renderImage = (imagePath, title) => {
-    const cxname = 'image';
+    const cxsize = this.state.clicked ? cx('lrg') : cx('sml');
+    const cximage = cx('image');
     return (
       <img
-        className={ cxname }
+        className={ [ cximage, cxsize ].join(' ') }
+        onClick={ this.handleClick }
         alt={ title }
         src={ imagePath }
         role='presentation' />
     );
+  };
+
+  handleClick = e => {
+    e.stopPropagation();
+    this.setState(prevState => ({
+      clicked: !prevState.clicked
+    }));
   };
 
   renderContext = (ctx, i) => {
