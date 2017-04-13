54df48e0c6b4cf7c52fecf10e7208ef3132dfdf9
Adam Gruber <talknmime@gmail.com>
2017-04-13 16:37:51 -0400

    migrate PropTypes to prop-types package (#25) [ci skip]
    * migrate PropTypes to prop-types package
    
    * update travis script to install react-test-renderer

--- src/components/summary/index.jsx
+++ src/components/summary/index.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Duration } from 'components';
 import classNames from 'classnames/bind';
 import styles from './summary.css';
