9c2aef9701b841484565b7b6fc261d641c827cd1
Adam Gruber <adamg@aweber.com>
2017-06-28 17:06:13 -0400

    context: dont highlight simple strings

--- test/spec/components/test/context.test.jsx
+++ test/spec/components/test/context.test.jsx
@@ -21,143 +21,152 @@ describe('<TestContext />', () => {
     };
   };
 
-  it('renders context when value is undefined', () => {
-    const context = {
-      title: 'sample context',
-      value: 'undefined'
-    };
-    const { wrapper, ctxItems } = getInstance({
-      context: JSON.stringify(context),
-      className: 'test'
+  describe('when context is a string', () => {
+    it('renders simple string', () => {
+      const context = 'sample context';
+      const { wrapper, snippet } = getInstance({
+        context: JSON.stringify(context),
+        className: 'test'
+      });
+      expect(wrapper).to.have.className('test');
+      expect(snippet).to.have.lengthOf(1);
     });
-    expect(wrapper).to.have.className('test');
-    expect(ctxItems).to.have.lengthOf(1);
+
+    it('renders url with protocol', () => {
+      const context = 'http://test.url.com/somepath';
+      const { wrapper, snippet, link } = getInstance({
+        context: JSON.stringify(context),
+        className: 'test'
+      });
+      expect(wrapper).to.have.className('test');
+      expect(snippet).to.have.lengthOf(0);
+      expect(link).to.have.lengthOf(1);
+      link.simulate('click', { stopPropagation: noop });
+    });
+
+    it('renders url without protocol', () => {
+      const context = 'test.url.com/somepath';
+      const { wrapper, snippet, link } = getInstance({
+        context: JSON.stringify(context),
+        className: 'test'
+      });
+      expect(wrapper).to.have.className('test');
+      expect(snippet).to.have.lengthOf(0);
+      expect(link).to.have.lengthOf(1);
+    });
+
+    it('renders image url with protocol', () => {
+      const context = 'http://test.url.com/testimage.png';
+      const { wrapper, snippet, img } = getInstance({
+        context: JSON.stringify(context),
+        className: 'test'
+      });
+      expect(wrapper).to.have.className('test');
+      expect(snippet).to.have.lengthOf(0);
+      expect(img).to.have.lengthOf(1);
+      img.simulate('click', { stopPropagation: noop });
+    });
+
+    it('renders image url without protocol', () => {
+      const context = 'test.url.com/testimage.png';
+      const { wrapper, snippet, img } = getInstance({
+        context: JSON.stringify(context),
+        className: 'test'
+      });
+      expect(wrapper).to.have.className('test');
+      expect(snippet).to.have.lengthOf(0);
+      expect(img).to.have.lengthOf(1);
+    });
+
+    it('renders local image', () => {
+      const context = '/testimage.png';
+      const { wrapper, snippet, img } = getInstance({
+        context: JSON.stringify(context),
+        className: 'test'
+      });
+      expect(wrapper).to.have.className('test');
+      expect(snippet).to.have.lengthOf(0);
+      expect(img).to.have.lengthOf(1);
+    });
   });
 
-  it('renders context with string', () => {
-    const context = 'sample context';
-    const { wrapper, snippet } = getInstance({
-      context: JSON.stringify(context),
-      className: 'test'
+  describe('when context is an object', () => {
+    it('renders undefined value', () => {
+      const context = {
+        title: 'sample context',
+        value: 'undefined'
+      };
+      const { wrapper, ctxItems } = getInstance({
+        context: JSON.stringify(context),
+        className: 'test'
+      });
+      expect(wrapper).to.have.className('test');
+      expect(ctxItems).to.have.lengthOf(1);
     });
-    expect(wrapper).to.have.className('test');
-    expect(snippet).to.have.lengthOf(1);
-  });
 
-  it('renders context with string, url with protocol', () => {
-    const context = 'http://test.url.com/somepath';
-    const { wrapper, snippet, link } = getInstance({
-      context: JSON.stringify(context),
-      className: 'test'
+
+    it('renders string value', () => {
+      const context = {
+        title: 'sample context',
+        value: 'context string'
+      };
+      const { wrapper, snippet } = getInstance({
+        context: JSON.stringify(context),
+        className: 'test'
+      });
+      expect(wrapper).to.have.className('test');
+      expect(snippet).to.have.lengthOf(1);
+      expect(snippet.prop('highlight')).to.equal(false);
     });
-    expect(wrapper).to.have.className('test');
-    expect(snippet).to.have.lengthOf(0);
-    expect(link).to.have.lengthOf(1);
-    link.simulate('click', { stopPropagation: noop });
-  });
 
-  it('renders context with string, url without protocol', () => {
-    const context = 'test.url.com/somepath';
-    const { wrapper, snippet, link } = getInstance({
-      context: JSON.stringify(context),
-      className: 'test'
-    });
-    expect(wrapper).to.have.className('test');
-    expect(snippet).to.have.lengthOf(0);
-    expect(link).to.have.lengthOf(1);
-  });
-
-  it('renders context with string, image url with protocol', () => {
-    const context = 'http://test.url.com/testimage.png';
-    const { wrapper, snippet, img } = getInstance({
-      context: JSON.stringify(context),
-      className: 'test'
-    });
-    expect(wrapper).to.have.className('test');
-    expect(snippet).to.have.lengthOf(0);
-    expect(img).to.have.lengthOf(1);
-    img.simulate('click', { stopPropagation: noop });
-  });
-
-  it('renders context with string, image url without protocol', () => {
-    const context = 'test.url.com/testimage.png';
-    const { wrapper, snippet, img } = getInstance({
-      context: JSON.stringify(context),
-      className: 'test'
-    });
-    expect(wrapper).to.have.className('test');
-    expect(snippet).to.have.lengthOf(0);
-    expect(img).to.have.lengthOf(1);
-  });
-
-  it('renders context with string, local image', () => {
-    const context = '/testimage.png';
-    const { wrapper, snippet, img } = getInstance({
-      context: JSON.stringify(context),
-      className: 'test'
-    });
-    expect(wrapper).to.have.className('test');
-    expect(snippet).to.have.lengthOf(0);
-    expect(img).to.have.lengthOf(1);
-  });
-
-  it('renders context with object, string value', () => {
-    const context = {
-      title: 'sample context',
-      value: 'context string'
-    };
-    const { wrapper, snippet } = getInstance({
-      context: JSON.stringify(context),
-      className: 'test'
-    });
-    expect(wrapper).to.have.className('test');
-    expect(snippet).to.have.lengthOf(1);
-  });
-
-  it('renders context with object, image url value', () => {
-    const context = {
-      title: 'sample context',
-      value: 'http://test.url.com/testimage.png'
-    };
-    const { wrapper, snippet, img } = getInstance({
-      context: JSON.stringify(context),
-      className: 'test'
-    });
-    expect(wrapper).to.have.className('test');
-    expect(snippet).to.have.lengthOf(0);
-    expect(img).to.have.lengthOf(1);
-  });
-
-  it('renders context with object, object value', () => {
-    const context = {
-      title: 'sample context',
-      value: { testing: true }
-    };
-    const { wrapper, snippet } = getInstance({
-      context: JSON.stringify(context),
-      className: 'test'
-    });
-    expect(wrapper).to.have.className('test');
-    expect(snippet).to.have.lengthOf(1);
-  });
-
-  it('renders context with array', () => {
-    const context = [
-      {
-        title: 'sample context a',
+    it('renders image url value', () => {
+      const context = {
+        title: 'sample context',
         value: 'http://test.url.com/testimage.png'
-      },
-      {
-        title: 'sample context b',
-        value: { testing: true }
-      }
-    ];
-    const { wrapper, snippet, img } = getInstance({
-      context: JSON.stringify(context),
-      className: 'test'
+      };
+      const { wrapper, snippet, img } = getInstance({
+        context: JSON.stringify(context),
+        className: 'test'
+      });
+      expect(wrapper).to.have.className('test');
+      expect(snippet).to.have.lengthOf(0);
+      expect(img).to.have.lengthOf(1);
     });
-    expect(wrapper).to.have.className('test');
-    expect(snippet).to.have.lengthOf(1);
-    expect(img).to.have.lengthOf(1);
+
+    it('renders object value', () => {
+      const context = {
+        title: 'sample context',
+        value: { testing: true }
+      };
+      const { wrapper, snippet } = getInstance({
+        context: JSON.stringify(context),
+        className: 'test'
+      });
+      expect(wrapper).to.have.className('test');
+      expect(snippet).to.have.lengthOf(1);
+      expect(snippet.prop('highlight')).to.equal(true);
+    });
+  });
+
+  describe('when context is an array', () => {
+    it('renders', () => {
+      const context = [
+        {
+          title: 'sample context a',
+          value: 'http://test.url.com/testimage.png'
+        },
+        {
+          title: 'sample context b',
+          value: { testing: true }
+        }
+      ];
+      const { wrapper, snippet, img } = getInstance({
+        context: JSON.stringify(context),
+        className: 'test'
+      });
+      expect(wrapper).to.have.className('test');
+      expect(snippet).to.have.lengthOf(1);
+      expect(img).to.have.lengthOf(1);
+    });
   });
 });
