da960649e7f6a7125b2c5d4c05ba1aa83c538a91
Adam Gruber <talknmime@gmail.com>
2017-10-25 14:57:03 -0400

    Feature/loader (#54)
    * 2.2.2 [ci skip]
    
    * Add ReportBody and Loader components
    
    * Add isLoading prop and toggleIsLoading method to store
    
    * Wire up new ReportBody component
    
    * Fix broken test

---
+++ test/spec/components/report-body/loader.test.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import chai, { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+
+import Loader from 'components/report-body/loader';
+
+chai.use(chaiEnzyme());
+
+describe('<Loader />', () => {
+  const getInstance = instanceProps => {
+    const wrapper = shallow(<Loader />);
+    return {
+      wrapper,
+    };
+  };
+
+  it('renders', () => {
+    const { wrapper } = getInstance();
+    expect(wrapper.find('.loader-text')).to.have.lengthOf(1);
+  });
+});
