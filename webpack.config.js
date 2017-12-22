b4cbebfc66d7f85a94720d88a89b97a27bb8f543
Adam Gruber <adamg@aweber.com>
2017-12-22 14:22:36 -0500

    combine webpack configs into single file

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,28 @@
-/* eslint-disable */
 const path = require('path');
 const webpack = require('webpack');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
-const pkg = require('../package.json');
+const loaders = require('./webpack.loaders');
+const pkg = require('./package.json');
+
+const { eslint, babel, globalCss, localCss, font } = loaders;
 
 const env = (!!process.env.BABEL_ENV && process.env.BABEL_ENV) ||
           (!!process.env.NODE_ENV && process.env.NODE_ENV) ||
           'development';
+let assets = process.env.MARGE_ASSETS;
+if (assets !== 'inline' && assets !== 'external') {
+  assets = 'external';
+}
 
 const isDev = env === 'development';
-const publicPath = isDev ? 'http://localhost:8080/' : '';
-const devtool = isDev ? 'source-map' : '';
+const fontLimit = assets === 'external' ? 1000 : 100000;
+const cssFilename = assets === 'inline' ? '[name].inline.css' : '[name].css';
 
 const plugins = [
   new StyleLintPlugin({ context: 'src', files: '**/*.css', color: true, emitErrors: false }),
-  new ExtractTextPlugin({ filename: '[name].css', allChunks: true }),
+  new ExtractTextPlugin({ filename: cssFilename, allChunks: true }),
   new webpack.BannerPlugin(`mochawesome-report-generator ${pkg.version} | https://github.com/adamgruber/mochawesome-report-generator`)
 ];
 
@@ -38,27 +44,51 @@ if (env === 'production') {
 }
 
 module.exports = {
-  env,
-  version: pkg.version,
-  baseConfig: {
-    devtool,
-    entry: {
-      app: './src/js/mochawesome.js'
-    },
-    output: {
-      path: path.resolve(__dirname, '..', 'dist', 'assets'),
-      publicPath,
-      filename: '[name].js'
-    },
-    resolve: {
-      extensions: [ '.js', '.json', '.jsx', '.css' ],
-      modules: [
-        'node_modules',
-        'src',
-        'components',
-        'styles'
-      ]
-    },
-    plugins
-  }
+  devServer: {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+    }
+  },
+  devtool: isDev ? 'source-map' : '',
+  entry: {
+    app: './src/js/mochawesome.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: isDev ? 'http://localhost:8080/' : '',
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: [ '.js', '.json', '.jsx', '.css' ],
+    modules: [
+      'node_modules',
+      'src',
+      'components',
+      'styles'
+    ]
+  },
+  module: {
+    rules: [
+      eslint({ enforce: 'pre' }),
+      babel({}, pkg.version),
+      globalCss({
+        minimize: true,
+        importLoaders: 1,
+        sourceMap: isDev
+      }),
+      localCss({
+        minimize: true,
+        modules: true,
+        importLoaders: 1,
+        localIdentName: isDev ? '[name]--[local]' : '[name]--[local]---[hash:base64:5]',
+        sourceMap: isDev
+      }),
+      font('woff', { limit: fontLimit }),
+      font('ttf', { limit: fontLimit }),
+      font('svg', { limit: fontLimit })
+    ]
+  },
+  plugins
 };
