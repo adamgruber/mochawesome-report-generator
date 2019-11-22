8f36c6fb41b5e45de3b81b132a044cf0dc2f5948
Adam Gruber <adamg@aweber.com>
2019-11-22 13:21:59 -0500

    Add quick summary component tests to fix coverage

--- test/spec/components/quick-summary.test.js
+++ test/spec/components/quick-summary.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import chai, { expect } from 'chai';
 import chaiEnzyme from 'chai-enzyme';
+import sinon from 'sinon';
 
 import QuickSummary from 'components/quick-summary';
 
@@ -15,11 +16,14 @@ describe('<QuickSummary />', () => {
     return {
       wrapper,
       lists: wrapper.find('.quick-summary-list'),
+      filterBtns: wrapper.find('button'),
     };
   };
 
   beforeEach(() => {
     props = {
+      onQuickFilterClick: sinon.spy(),
+      singleFilter: null,
       stats: {
         duration: 532,
         suites: 14,
@@ -47,4 +51,23 @@ describe('<QuickSummary />', () => {
     expect(lists.at(0).find('.quick-summary-item')).to.have.lengthOf(3);
     expect(lists.at(1).find('.quick-summary-item')).to.have.lengthOf(2);
   });
+
+  it('renders with `singleFilter` set', () => {
+    props.singleFilter = 'showPassed';
+    const { lists } = getInstance(props);
+    expect(lists.at(1)).to.have.className('single-filter');
+    expect(lists.at(1)).to.have.className('single-filter--passed');
+  });
+
+  describe('quick filters', () => {
+    it('should call `onQuickFilterClick` with expected argument', () => {
+      const { filterBtns } = getInstance(props);
+      const filters = ['showPassed', 'showFailed', 'showPending', 'showSkipped'];
+      filterBtns.forEach((btn, i) => {
+        btn.simulate('click');
+        expect(props.onQuickFilterClick.getCall(i).args[0]).to.equal(filters[i]);
+      })
+    });
+  });
+
 });
