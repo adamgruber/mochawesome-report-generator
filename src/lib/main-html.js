8debab261a8c9ad194b29bf90a956404dfb56f56
Adam Gruber <adamg@aweber.com>
2020-04-13 11:58:08 -0400

    Convert MainHtml from component to function
    Removes JSX markup and React. Replaces with a function that uses
    template strings to render the main html markup to a string

--- src/lib/main-html.js
+++ src/lib/main-html.js
@@ -1,8 +1,33 @@
-/* eslint-disable react/no-danger */
-const React = require('react');
-const PropTypes = require('prop-types');
+const escapeHtml = require('escape-html');
 
-function MainHTML(props) {
+/**
+ * Escape entities for use in HTML
+ *
+ * @param {string} str Input string
+ *
+ * @return {string}
+ */
+function e(str) {
+  return escapeHtml(str).replace(/&#39/g, '&#x27')
+}
+
+/**
+ * Render the main report HTML to a string
+ *
+ * @param {object} props Report properties
+ * @param {string} data Raw report data
+ * @param {string} inlineScripts App JS
+ * @param {string} inlineStyles App CSS
+ * @param {object} options App options
+ * @param {string} scriptsUrl URL for app JS
+ * @param {string} stylesUrl URL for app CSS
+ * @param {string} title Report page title
+ * @param {boolean} useInlineAssets Whether to render JS/CSS inline
+ *
+ * @return {string}
+ */
+
+function renderMainHTML(props) {
   const {
     data,
     inlineScripts,
@@ -13,43 +38,25 @@ function MainHTML(props) {
     title,
     useInlineAssets,
   } = props;
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>{title}</title>
-        {useInlineAssets ? (
-          <style dangerouslySetInnerHTML={{ __html: inlineStyles }} />
-        ) : (
-          <link rel="stylesheet" href={stylesUrl} />
-        )}
-      </head>
-      <body data-raw={data} data-config={JSON.stringify(options)}>
-        <div id="report" />
-        {useInlineAssets ? (
-          <script
-            type="text/javascript"
-            dangerouslySetInnerHTML={{ __html: inlineScripts }}
-          />
-        ) : (
-          <script src={scriptsUrl} />
-        )}
-      </body>
-    </html>
-  );
+
+  const styles = useInlineAssets
+    ? `<style>${inlineStyles}</style>`
+    : `<link rel="stylesheet" href="${stylesUrl}"/>`;
+
+  const scripts = useInlineAssets
+    ? `<script type="text/javascript">${inlineScripts}</script>`
+    : `<script src="${scriptsUrl}"></script>`;
+
+  const meta =
+    '<meta charSet="utf-8"/><meta http-equiv="X-UA-Compatible" content="IE=edge"/><meta name="viewport" content="width=device-width, initial-scale=1"/>';
+
+  const head = `<head>${meta}<title>${e(title)}</title>${styles}</head>`;
+
+  const body = `<body data-raw="${e(data)}" data-config="${e(
+    JSON.stringify(options)
+  )}"><div id="report"></div>${scripts}</body>`;
+
+  return `<html lang="en">${head}${body}</html>`;
 }
 
-MainHTML.propTypes = {
-  data: PropTypes.string,
-  inlineScripts: PropTypes.string,
-  inlineStyles: PropTypes.string,
-  options: PropTypes.object,
-  scriptsUrl: PropTypes.string,
-  stylesUrl: PropTypes.string,
-  title: PropTypes.string,
-  useInlineAssets: PropTypes.bool,
-};
-
-module.exports = MainHTML;
+module.exports = renderMainHTML;
